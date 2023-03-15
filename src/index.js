4afa605cdc10843e9104113f71c19275d6b2230a
varya <varyadaser@gmail.com>
2023-03-15 15:47:21 +0400

    fixed misspelled filepaths :gear:

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 const audioCtx = new AudioContext();
-const audio = new Audio("./galasavoner/galasavo.mp3");
-const audio2 = new Audio("./galasavo/galasavo2.mp3");
+const audio = new Audio("../galasavoner/galasavo.mp3");
+const audio2 = new Audio("../galasavoner/galasavo2.mp3");
 
 const source = audioCtx.createMediaElementSource(audio);
 source.connect(audioCtx.destination);
 
 const source2 = audioCtx.createMediaElementSource(audio2);
 source2.connect(audioCtx.destination);
-
 
 const btn = document.createElement("button");
 btn.innerText = "klik";
