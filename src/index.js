966592a16ec86a1a26c99b202c8d4e7666c1a1ea
varya <varyadaser@gmail.com>
2023-03-17 10:27:45 +0400

    mixed buffers insted of playing simultaneously :axe:

--- src/index.js
+++ src/index.js
@@ -2,20 +2,55 @@ const audioCtx = new AudioContext();
 const audio = new Audio("../galasavoner/galasavo.mp3");
 const audio2 = new Audio("../galasavoner/galasavo2.mp3");
 
-const source = audioCtx.createMediaElementSource(audio);
-source.connect(audioCtx.destination);
+const buffer1 = audioCtx.decodeAudioData(audio)
+const buffer2 = audioCtx.decodeAudioData(audio2)
 
-const source2 = audioCtx.createMediaElementSource(audio2);
-source2.connect(audioCtx.destination);
 
-const playBoth = () => {
-  audio.play();
-  audio2.play();
-};
+function mix(buffers, context) {
+  var maxChannels = 0;
+  var maxDuration = 0;
+  for (let i = 0; i < buffers.length; i++) {
+    if (buffers[i].numberOfChannels > maxChannels) {
+      maxChannels = buffers[i].numberOfChannels;
+    }
+    if (buffers[i].duration > maxDuration) {
+      maxDuration = buffers[i].duration;
+    }
+  }
+  var out = context.createBuffer(
+    maxChannels,
+    context.sampleRate * maxDuration,
+    context.sampleRate
+  );
+
+  for (var j = 0; j < buffers.length; j++) {
+    for (
+      var srcChannel = 0;
+      srcChannel < buffers[j].numberOfChannels;
+      srcChannel++
+    ) {
+      var output = out.getChanneData(srcChannel);
+      var input = buffers[j].getChanneData(srcChannel);
+      for (let i = 0; i < input.length; i++) {
+        output[i] += input[i];
+      }
+    }
+  }
+  return out;
+}
+
+
+const buffers = [buffer1, buffer2]
+
+const source = audioContext.createBufferSource();
+const newBuffer = mix(buffers, audioCtx)
+
+source.buffer = newBuffer;
+source.connect(audioCtx.destination)
 
 const btn = document.createElement("button");
 btn.innerText = "klik";
 btn.addEventListener("click", () => {
-  playBoth();
+  source.start()
 });
 document.body.appendChild(btn);
